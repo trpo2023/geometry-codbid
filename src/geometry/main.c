536afb41c6b8b5d6759ae5a07bd4326a2ead2f1a
Даниил Истомин <istomin.05.3@gmail.com>
2023-04-09 20:11:57 +0700

    Implement output a perimeter and area of circle

--- src/geometry/main.c
+++ src/geometry/main.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "../libgeometry/parser.h"
+#define PI 3.14
 #define MAX_BUFFER_SIZE 127
 
 int main(int argc, char* argv[])
@@ -19,8 +21,20 @@ int main(int argc, char* argv[])
     }
     while (!feof(fp)) {
         fgets(buffer, 127, fp);
-        if (is_Correct(buffer) == 1) {
-            printf("Incorrect str: '%s'\n", strtok(buffer, "\n"));
+        //printf("Incorrect str: '%s'\n", strtok(buffer, "\n"));
+        Circle* circle = parse(buffer);
+        if ( circle->error_index )
+        {
+            printf("Error in column %d:\n", circle->error_index);
+            printf("%s\n", strtok(buffer, "\n"));
+            printf("%*s%c\n\n", circle->error_index, "", '^');
+        } 
+        else 
+        {
+            printf("%s\n", strtok(buffer, "\n"));
+            printf("Area = %.2lf\n", PI * (circle->r * circle->r));
+            printf("Perimeter = %.2lf\n\n", 2 * PI * circle->r);
         }
     }
 }
+
